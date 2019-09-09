ffbcbfdd0ea475bb45058c523c1d33e96f6618a0
Filip Jerga <jerga99@gmail.com>
2019-09-10 00:58:51 +0200

    Adds Image

--- components/movieList.js
+++ components/movieList.js
@@ -1,8 +1,8 @@
 import React from 'react'
 
 const MovieList = (props) => {
-
   const { movies } = props
+  const shorten = (text) => text.substr(0, 100) + '...'
   return (
     <React.Fragment>
       { movies.map(movie => {
@@ -10,14 +10,14 @@ const MovieList = (props) => {
           <div key={movie.id} className="col-lg-4 col-md-6 mb-4">
             <div className="card h-100">
               <a href="#">
-                <img className="card-img-top" src="http://placehold.it/700x400" alt="" />
+                <img className="card-img-top img-fluid" src={movie.image} alt="" />
               </a>
               <div className="card-body">
                 <h4 className="card-title">
                   <a href="#">{movie.name}</a>
                 </h4>
                 <h5>{movie.genre}</h5>
-                <p className="card-text">{movie.description}</p>
+                <p className="card-text">{shorten(movie.description)}</p>
               </div>
               <div className="card-footer">
                 <small className="text-muted">{movie.rating}</small>
