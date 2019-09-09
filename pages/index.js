9d6ec3324db4f5f6f1f24826677f227a075f6901
Filip Jerga <jerga99@gmail.com>
2019-09-09 13:52:50 +0200

    4 Movie List

--- pages/index.js
+++ pages/index.js
@@ -5,18 +5,45 @@ import Nav from '../components/nav'
 import SideMenu from '../components/sideMenu'
 import Carousel from '../components/Carousel'
 import MovieList from '../components/MovieList'
+import Footer from '../components/footer'
+
+const movieData = [
+  {
+    id: '1',
+    name: 'The Shawshank Redemption',
+    releaseYear: 1994,
+    description: 'Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.',
+    rating: 4.8,
+    genre: 'drama'
+  },
+  {
+    id: '2',
+    name: 'The Dark Knight',
+    releaseYear: 2008,
+    description: 'When the menace known as The Joker emerges from his mysterious past, he wreaks havoc and chaos on the people of Gotham. The Dark Knight must accept one of the greatest psychological and physical tests of his ability to fight injustice.',
+    rating: 4.7,
+    genre: 'action, crime, drama'
+  },
+  {
+    id: '3',
+    name: 'The Dark Knight',
+    releaseYear: 2008,
+    description: 'When the menace known as The Joker emerges from his mysterious past, he wreaks havoc and chaos on the people of Gotham. The Dark Knight must accept one of the greatest psychological and physical tests of his ability to fight injustice.',
+    rating: 4.7,
+    genre: 'adventure, drama, fantasy'
+  }
+]
 
 const Home = () => (
   <div>
     <Head>
       <title>Home</title>
-      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous" />
-      <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossOrigin="anonymous" />
+      <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossOrigin="anonymous"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossOrigin="anonymous"></script>
+      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossOrigin="anonymous"></script>
     </Head>
   <Nav />
-
   <div className="home-page">
     <div className="container">
       <div className="row">
@@ -26,17 +53,13 @@ const Home = () => (
         <div className="col-lg-9">
           <Carousel />
           <div className="row">
-            <MovieList />
+            <MovieList movies={movieData} />
           </div>
         </div>
       </div>
     </div>
   </div>
-  <footer className="py-5 bg-dark">
-    <div className="container">
-      <p className="m-0 text-center text-white">Copyright &copy; Your Website 2019</p>
-    </div>
-  </footer>
+  <Footer />
   <style jsx>{`
     .home-page {
       margin-top: 80px;
