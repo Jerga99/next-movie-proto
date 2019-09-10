c07eea02bc76b803da1b47b5e54634f1de5858e2
Filip Jerga <jerga99@gmail.com>
2019-09-10 19:15:15 +0200

    23 Adds Modal

--- components/sideMenu.js
+++ components/sideMenu.js
@@ -1,4 +1,4 @@
-
+import Modal from './modal'
 
 
 const SideMenu = (props) => {
@@ -14,6 +14,7 @@ const SideMenu = (props) => {
           ))
         }
       </div>
+      <Modal />
    </div>
   )
 }
