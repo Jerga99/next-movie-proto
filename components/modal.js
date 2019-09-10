175301dd046c52fcbf097d9974a26c3079162625
Filip Jerga <jerga99@gmail.com>
2019-09-10 20:20:58 +0200

    26 Containment

--- components/modal.js
+++ components/modal.js
@@ -1,7 +1,7 @@
 
 
 
-const Modal = () => {
+const Modal = (props) => {
 
   let closeButton = null
 
@@ -27,43 +27,7 @@ const Modal = () => {
               </button>
             </div>
             <div class="modal-body">
-              <form>
-                <div class="form-group">
-                  <label for="name">Name</label>
-                  <input type="text" class="form-control" id="name" aria-describedby="emailHelp" placeholder="Lord of the Rings" />
-                </div>
-                <div class="form-group">
-                  <label for="description">Description</label>
-                  <input type="text" class="form-control" id="description" placeholder="Somewhere in Middle-earth..." />
-                </div>
-                <div class="form-group">
-                  <label for="description">Rating</label>
-                  <input type="number" max="5" min="0" class="form-control" id="rating" placeholder="3" />
-                  <small id="emailHelp" class="form-text text-muted">Max: 5, Min: 0 </small>
-                </div>
-                <div class="form-group">
-                  <label for="image">Image</label>
-                  <input type="text" class="form-control" id="image" placeholder="http://....." />
-                </div>
-                <div class="form-group">
-                  <label for="cover">Cover</label>
-                  <input type="text" class="form-control" id="cover" placeholder="http://......" />
-                </div>
-                <div class="form-group">
-                  <label for="longDesc">Long Description</label>
-                  <textarea class="form-control" id="longDesc" rows="3"></textarea>
-                </div>
-                <div class="form-group">
-                  <label for="genre">Genre</label>
-                  <select multiple class="form-control" id="genre">
-                    <option>drama</option>
-                    <option>music</option>
-                    <option>adventure</option>
-                    <option>historical</option>
-                    <option>action</option>
-                  </select>
-                </div>
-              </form>
+              {props.children}
             </div>
             <div class="modal-footer">
               <button ref={(ele) => { closeButton = ele }} type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
--- components/sideMenu.js
+++ components/sideMenu.js
@@ -1,10 +1,12 @@
 import Modal from './modal'
-
+import ModalCreateForm from './movieCreateForm'
 
 const SideMenu = (props) => {
   return (
    <div>
-     <Modal />
+     <Modal>
+       <ModalCreateForm />
+     </Modal>
      <h1 className="my-4">Categories</h1>
       <div className="list-group">
         { props.categories.map(c => (
