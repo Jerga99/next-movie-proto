86d70fa6fb72b8071465dd76d8c6840dd5fa1629
Filip Jerga <jerga99@gmail.com>
2019-09-10 19:02:48 +0200

    22 Add Links

--- components/movieList.js
+++ components/movieList.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Link from 'next/link'
 
 const MovieList = (props) => {
   const { movies } = props
@@ -9,12 +10,16 @@ const MovieList = (props) => {
         return (
           <div key={movie.id} className="col-lg-4 col-md-6 mb-4">
             <div className="card h-100">
-              <a href="#">
-                <img className="card-img-top img-fluid" src={movie.image} alt="" />
-              </a>
+              <Link href={`/movies/${movie.id}`}>
+                <a>
+                  <img className="card-img-top img-fluid" src={movie.image} alt="" />
+                </a>
+              </Link>
               <div className="card-body">
                 <h4 className="card-title">
-                  <a href="#">{movie.name}</a>
+                  <Link href={`/movies/${movie.id}`}>
+                    <a href="#">{movie.name}</a>
+                  </Link>
                 </h4>
                 <h5>{movie.genre}</h5>
                 <p className="card-text">{shorten(movie.description)}</p>
