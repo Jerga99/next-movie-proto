91c3a70e8eae7a3e9b2915f7b7765f6440528e37
Filip Jerga <jerga99@gmail.com>
2019-09-10 20:44:21 +0200

    27 Handles form submit

--- components/movieCreateForm.js
+++ components/movieCreateForm.js
@@ -1,34 +1,35 @@
-
+import { useState, useEffect } from 'react'
 
 
 const MovieCreateForm = () => {
+  const [form, setForm] = useState({})
 
   return (
     <form>
       <div class="form-group">
         <label for="name">Name</label>
-        <input type="text" class="form-control" id="name" aria-describedby="emailHelp" placeholder="Lord of the Rings" />
+        <input value={form.name} type="text" class="form-control" id="name" aria-describedby="emailHelp" placeholder="Lord of the Rings" />
       </div>
       <div class="form-group">
         <label for="description">Description</label>
-        <input type="text" class="form-control" id="description" placeholder="Somewhere in Middle-earth..." />
+        <input value={form.description} type="text" class="form-control" id="description" placeholder="Somewhere in Middle-earth..." />
       </div>
       <div class="form-group">
         <label for="description">Rating</label>
-        <input type="number" max="5" min="0" class="form-control" id="rating" placeholder="3" />
+        <input value={form.rating} type="number" max="5" min="0" class="form-control" id="rating" placeholder="3" />
         <small id="emailHelp" class="form-text text-muted">Max: 5, Min: 0 </small>
       </div>
       <div class="form-group">
         <label for="image">Image</label>
-        <input type="text" class="form-control" id="image" placeholder="http://....." />
+        <input value={form.image} type="text" class="form-control" id="image" placeholder="http://....." />
       </div>
       <div class="form-group">
         <label for="cover">Cover</label>
-        <input type="text" class="form-control" id="cover" placeholder="http://......" />
+        <input value={form.cover} type="text" class="form-control" id="cover" placeholder="http://......" />
       </div>
       <div class="form-group">
         <label for="longDesc">Long Description</label>
-        <textarea class="form-control" id="longDesc" rows="3"></textarea>
+        <textarea value={form.longDesc} class="form-control" id="longDesc" rows="3"></textarea>
       </div>
       <div class="form-group">
         <label for="genre">Genre</label>
@@ -40,6 +41,7 @@ const MovieCreateForm = () => {
           <option>action</option>
         </select>
       </div>
+      <button onClick={() => alert(JSON.stringify(form))}>Submit</button>
     </form>
   )
 }
