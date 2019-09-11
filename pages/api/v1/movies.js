8a38ba56af6e2cea1b25fe181c972a945233ac58
Filip Jerga <jerga99@gmail.com>
2019-09-11 22:09:45 +0200

    40 Adds Data differently

--- pages/api/v1/movies.js
+++ pages/api/v1/movies.js
@@ -1,5 +1,33 @@
-// export { default } from '../../../server/data.json'
 
-// export default (req, res) => {
-//   return res.json(data)
-// }
+import fs from 'fs'
+import path from 'path'
+const filePath = '../../../server/data.json'
+import data from filePath
+
+export default (req, res) => {
+  const { method } = req
+
+   switch (method) {
+    case 'GET':
+      res.status(200).json(data)
+      break
+    case 'POST':
+      const movie = req.body
+      data.push(movie)
+
+      const pathToFile = path.join(__dirname, filePath)
+      const stringifiedData = JSON.stringify(data, null, 2)
+
+      fs.writeFile(pathToFile, stringifiedData, function(err) {
+        if (err) {
+          return res.status(422).send(err)
+        }
+
+        return res.json('File Sucesfully updated')
+      })
+      break
+    default:
+      res.setHeader('Allow', ['GET', 'PUT'])
+      res.status(405).end(`Method ${method} Not Allowed`)
+  }
+}
