a58bd3a6f1510d25ac8e5102a75a935dd5fb6160
Filip Jerga <jerga99@gmail.com>
2019-09-11 12:11:16 +0200

    33 Server rework

--- server/index.js
+++ server/index.js
@@ -2,19 +2,23 @@
 const { createServer } = require('http')
 const { parse } = require('url')
 const next = require('next')
+const express = require('express');
 
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
 
 app.prepare().then(() => {
-  const server = createServer((req, res) => {
 
-  const parsedUrl = parse(req.url, true)
-  handle(req, res, parsedUrl)
+  const server = express();
+  server.get('*', (req, res) => {
+    return handle(req, res)
+  })
 
-  }).listen(3000, err => {
+  const PORT = process.env.PORT || 3000;
+
+  server.use(handle).listen(PORT, (err) => {
     if (err) throw err
-    console.log('> Ready on http://localhost:3000')
+    console.log('> Ready on port ' + PORT)
   })
 })
