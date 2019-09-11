42de0f8f37f8a2f98735ea7876cad3fbed719619
Filip Jerga <jerga99@gmail.com>
2019-09-11 22:56:28 +0200

    44 Fixes dynamic routing

--- components/movieList.js
+++ components/movieList.js
@@ -10,15 +10,15 @@ const MovieList = (props) => {
         return (
           <div key={movie.id} className="col-lg-4 col-md-6 mb-4">
             <div className="card h-100">
-              <Link href={`/movies/${movie.id}`}>
+              <Link href="/movies/[id]" as={`/movies/${movie.id}`}>
                 <a>
                   <img className="card-img-top img-fluid" src={movie.image} alt="" />
                 </a>
               </Link>
               <div className="card-body">
                 <h4 className="card-title">
-                  <Link href={`/movies/${movie.id}`}>
-                    <a href="#">{movie.name}</a>
+                  <Link href="/movies/[id]" as={`/movies/${movie.id}`}>
+                    <a>{movie.name}</a>
                   </Link>
                 </h4>
                 <h5>{movie.genre}</h5>
