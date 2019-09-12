3b31532c825738594a67c41fb395aad081797837
Filip Jerga <jerga99@gmail.com>
2019-09-12 14:22:44 +0200

    49 Fetch movie to edit page

--- pages/movies/[id]/edit.js
+++ pages/movies/[id]/edit.js
@@ -1,13 +1,30 @@
 
 
 import React from 'react'
-
+import { getMovieById } from '../../../actions'
 
 class Edit extends React.Component {
 
+  static getInitialProps({query}) {
+    return {query}
+  }
+
+  state = {
+    movie: {}
+  }
+
+  componentDidMount() {
+    const { id } = this.props.query
+    getMovieById(id).then((movie) => {
+      alert(JSON.stringify(movie))
+    })
+  }
+
   render() {
     return (
-      <h1>Hello Edit Page</h1>
+      <div className="container">
+        <h1>Edit the Movie</h1>
+      </div>
     )
   }
 }
